a2e719abca4251d508f6234d5249c1b0d5146c5b
Jeroen Stiekema <jeroen@stiekema.eu>
2019-12-11 21:29:32 +0100

    day 11, part 2

--- src/main/java/eu/stiekema/jeroen/adventofcode2019/day11/Day11.java
+++ src/main/java/eu/stiekema/jeroen/adventofcode2019/day11/Day11.java
@@ -13,17 +13,37 @@ import java.util.Map;
 
 public class Day11 {
 
-    public static final int BLACK = 0;
+    private static final Integer BLACK = 0;
+    private static final Integer WHITE = 1;
 
     public static void main(String[] args) throws IOException {
         List<Long> codes = FileParseUtil.getCodes("day11.txt", ",");
+        Map<Coordinate, Integer> paintedPanels = computePaintedPanels(codes, new Coordinate(0, 0), BLACK, Direction.UP);
+        System.out.println("Day 11, Answer 1: " + paintedPanels.size());
 
+        System.out.println("Answer 2:");
+        printPaintedPanels(computePaintedPanels(codes, new Coordinate(0, 0), WHITE, Direction.UP));
+
+
+    }
+
+    private static void printPaintedPanels(Map<Coordinate, Integer> paintedPanels) {
+        int minX = paintedPanels.keySet().stream().map(c -> c.x).min(Integer::compareTo).get();
+        int minY = paintedPanels.keySet().stream().map(c -> c.y).min(Integer::compareTo).get();
+        int maxX = paintedPanels.keySet().stream().map(c -> c.x).max(Integer::compareTo).get();
+        int maxY = paintedPanels.keySet().stream().map(c -> c.y).max(Integer::compareTo).get();
+
+        for (int y = minY; y <= maxY; y++) {
+            for (int x = minX; x <= maxX; x++) {
+                Integer color = paintedPanels.get(new Coordinate(x, y));
+                System.out.print(WHITE.equals(color) ? '\u258A' : ' ');
+            }
+            System.out.println();
+        }
+    }
+
+    private static Map<Coordinate, Integer> computePaintedPanels(List<Long> codes, Coordinate currentPanel, int currentPanelColor, Direction robotDirection) {
         Map<Coordinate, Integer> paintedPanels = new HashMap<>();
-
-        Direction robotDirection = Direction.UP;
-        Coordinate currentPanel = new Coordinate(0, 0);
-        int currentPanelColor = BLACK;
-
         try {
             IntcodeComputer intcodeComputer = IntcodeComputer.newInstance(codes);
             while (!intcodeComputer.isTerminated()) {
@@ -38,8 +58,7 @@ public class Day11 {
                 currentPanelColor = paintedPanels.getOrDefault(currentPanel, BLACK);
             }
         } catch (IntCodeComputerTerminatedException ignored) {}
-
-        System.out.println("Day 11, Answer 1: " + paintedPanels.size());
+        return paintedPanels;
     }
 
     private static Direction getNewDirection(Direction robotDirection, long directionIndication) {
